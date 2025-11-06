d5f0a0c49546e855f38be42942866d55635f6b72
tccmw <jungmwoo6248@gmail.com>
2025-11-06 14:52:00 +0900

    feat (#11) : header 높이 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Logo_fake from "../assets/logo_fake.svg";
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50 p-3 flex justify-between items-center">
-      <img src={Logo_fake} className="w-[5%]" />
+      <img src={Logo_fake} className="w-[3%]" />
     </header>
   );
 }
