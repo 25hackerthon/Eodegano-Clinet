abc07e7bc7f93e7507b8ccbe9f72791838f2d1b4
tccmw <jungmwoo6248@gmail.com>
2025-11-06 14:59:13 +0900

    feat (#17) : 라우터 경로 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./page/mainPage";
+import TemplatePage from "./page/templatePage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/Template" element={<TemplatePage />} />
       </Routes>
     </Router>
   );
