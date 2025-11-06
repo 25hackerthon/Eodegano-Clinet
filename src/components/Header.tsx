6e6488cad4063502d1414501e721da8a162bdce1
tccmw <jungmwoo6248@gmail.com>
2025-11-06 21:43:56 +0900

    refactor: logo 크기 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.svg";
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50 p-3 flex justify-between items-center">
-      <img src={logo} className="w-[10%]" />
+      <img src={logo} className="w-[5%] ml-[3%]" />
     </header>
   );
 }
