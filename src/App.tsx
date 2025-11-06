7c7a0d0ebfba191d437ecd7d536ba7e1e3957f83
이효준 <148470980+123isi@users.noreply.github.com>
2025-11-06 14:04:23 +0900

    Remove import of HomePage from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import HomePage from './pages/Home'
 import CreatePage from './pages/create'
 import './App.css'
 
